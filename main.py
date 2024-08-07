b3d4af37359f9698840c70b8706f069ea112b746
jessk <jessicakanik@gmail.com>
2024-08-07 13:36:29 +0100

    Lists

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print ("to do")
+user_prompt= "Enter a to-do: "
+
+to_do1 = input (user_prompt)
+to_do2 = input (user_prompt)
+to_do3 = input (user_prompt)
+to_do4 = input (user_prompt)
+
+to_dos = [to_do1,to_do2,to_do3,to_do4]
+
+print (to_dos)
